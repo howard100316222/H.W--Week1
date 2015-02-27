b14e3093034c485df057ef3641ec6d7489662409
howard711476 <100316222@gms.tcu.edu.tw>
2015-02-27 22:55:17 +0800

    更

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,10 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("月落烏啼霜滿天，\n");
+public class HelloWorld 
+{
+    public static void main(String[] args) 
+	{
+        System.out.println("月落烏啼霜滿天，");
 		System.out.println("江楓漁火對愁眠。\n");
-		System.out.println("姑蘇城外寒山寺，\n");
+		System.out.println("姑蘇城外寒山寺，");
 		System.out.println("夜半鐘聲到客船。\n");
     }
 }
